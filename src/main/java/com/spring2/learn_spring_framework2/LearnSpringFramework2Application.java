231c1a04be6954d8085f83d50f9d81b09d3c05dd
yeonjae <holmanyjd@gmail.com>
2024-08-31 22:07:38 +0900

    기본세팅

---
+++ src/main/java/com/spring2/learn_spring_framework2/LearnSpringFramework2Application.java
@@ -0,0 +1,13 @@
+package com.spring2.learn_spring_framework2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringFramework2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnSpringFramework2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring2/learn_spring_framework2/LearnSpringFramework2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring2.learn_spring_framework2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringFramework2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
